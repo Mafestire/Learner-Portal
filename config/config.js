99ead354868da2828a3c077a63888ed767573da4
Mafestire <seabaraven@gmail.com>
2023-03-17 11:31:43 +0200

    update - config

--- config/config.js
+++ config/config.js
@@ -1,15 +1,16 @@
 //import .env amd mysql
-require('dotenv').config();
-const {createConnection} = require('mysql');
+require("dotenv").config();
+const { createPool } = require("mysql");
 
 //connection
-let connect = createConnection({
-    host: process.env.DBhost,
-    user: process.env.DBuser,
-    password: process.env.DBpass,
-    port: process.env.DBport,
-    database: process.env.DBname,
-    multipleStatements: true
+let connect = createPool({
+  host: process.env.DBhost,
+  user: process.env.DBuser,
+  password: process.env.DBpass,
+  port: process.env.DBport,
+  database: process.env.DBname,
+  multipleStatements: true,
+  connectionLimit: 30,
 });
 
 module.exports = connect;
